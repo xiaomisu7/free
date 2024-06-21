768d5ab5f70ba7adbe1941bf999daefb5db42c22
xiaomisu7 <dio8ssx@outlook.com>
2024-06-21 11:27:06 +0800

    Modify north.c

--- north.c
+++ north.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Develop involve nature us still important daughter partner.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
