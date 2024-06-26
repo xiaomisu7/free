081366d1826d20ace29aee9b97ba3c0d612faf45
xiaomisu7 <dio8ssx@outlook.com>
2024-06-26 22:20:36 +0800

    Modify step.cpp

--- step.cpp
+++ step.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Evidence market unit same card.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
