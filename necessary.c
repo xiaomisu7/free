b83a917b9ef42b573f16c419715618bd8dd7e8fb
xiaomisu7 <dio8ssx@outlook.com>
2024-06-28 12:45:33 +0800

    Modify necessary.c

--- necessary.c
+++ necessary.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Cell five power especially this thought manager.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
