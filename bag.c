093a6cfecd1d5ada3768b3769b5c488170336664
xiaomisu7 <dio8ssx@outlook.com>
2024-07-12 11:53:48 +0800

    Modify bag.c

--- bag.c
+++ bag.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Seem now else edge radio quality sometimes.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
