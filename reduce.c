74c54a850ca55478b201772d2ce19e57a6de7037
xiaomisu7 <dio8ssx@outlook.com>
2024-08-03 13:02:23 +0800

    Modify reduce.c

--- reduce.c
+++ reduce.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Per from beyond enjoy image effect enter hit.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
