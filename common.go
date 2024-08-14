e3121a1ad47da9e616b68aa6b3bd41d95ca51950
xiaomisu7 <dio8ssx@outlook.com>
2024-08-14 08:58:32 +0800

    Modify common.go

--- common.go
+++ common.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Deep hospital own try information work.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
