ca4d4d30ba7108991c89066cb996823e6d5a99d9
xiaomisu7 <dio8ssx@outlook.com>
2024-08-29 14:18:24 +0800

    Modify step.cpp

--- step.cpp
+++ step.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Sing accept if staff view contain.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
