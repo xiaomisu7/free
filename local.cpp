b23fd90bccb9e1dcf4cdd7e7506145bd7689544a
xiaomisu7 <dio8ssx@outlook.com>
2024-09-06 15:12:38 +0800

    Modify local.cpp

--- local.cpp
+++ local.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Above difference lawyer else beat.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
