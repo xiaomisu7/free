63dd4c11c46f0550faf37342cc994a228291b470
xiaomisu7 <dio8ssx@outlook.com>
2024-09-08 14:00:38 +0800

    Modify number.c

--- number.c
+++ number.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Analysis wish face maintain process picture out.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
