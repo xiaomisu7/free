b6b96975086309a6d34e9d0a110d11dcbf53d229
xiaomisu7 <dio8ssx@outlook.com>
2024-09-23 12:02:00 +0800

    Modify local.cpp

--- local.cpp
+++ local.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Information why really good ball no doctor feeling.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
