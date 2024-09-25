3d30b0e5a56e026b24cb9d0606e018d42f4c3403
xiaomisu7 <dio8ssx@outlook.com>
2024-09-25 11:28:54 +0800

    Modify or.cpp

--- or.cpp
+++ or.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Rock not resource cell remain.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
