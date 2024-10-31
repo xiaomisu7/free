3f65083b7041b9438af11f4068b9641b024c378f
xiaomisu7 <dio8ssx@outlook.com>
2024-10-31 13:20:31 +0800

    Modify finish.cpp

--- finish.cpp
+++ finish.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Unit paper less practice.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
