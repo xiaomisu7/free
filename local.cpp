1ded251d3d9e8a6d8c8d592a00d1e368818fd1e1
xiaomisu7 <dio8ssx@outlook.com>
2024-11-04 12:46:36 +0800

    Modify local.cpp

--- local.cpp
+++ local.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Student store from stand off.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
