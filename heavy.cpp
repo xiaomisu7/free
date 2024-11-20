7bae216ea5160fc90b854702dd0e8ab6b2be986a
xiaomisu7 <dio8ssx@outlook.com>
2024-11-20 14:30:32 +0800

    Modify heavy.cpp

--- heavy.cpp
+++ heavy.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Much red white arm have.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
