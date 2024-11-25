7afdd712dd72238f36a9898a50ecb6c7b6be8729
xiaomisu7 <dio8ssx@outlook.com>
2024-11-25 16:39:20 +0800

    Modify ground.cpp

--- ground.cpp
+++ ground.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# News bill together marriage.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
