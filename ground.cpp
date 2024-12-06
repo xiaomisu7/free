74ca1058494e6fa6be8c18543d1e792c094ca77d
xiaomisu7 <dio8ssx@outlook.com>
2024-12-06 15:08:54 +0800

    Modify ground.cpp

--- ground.cpp
+++ ground.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Near agent environment growth order.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
