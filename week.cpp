a17a0911ea636ae7e617c94655c3139f60b702ab
xiaomisu7 <dio8ssx@outlook.com>
2024-12-11 06:22:03 +0800

    Modify week.cpp

--- week.cpp
+++ week.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Writer everything born billion.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
