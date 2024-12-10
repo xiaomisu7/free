7471ae8d65ca47bc4a7c3b6bd15363489ac80a2f
xiaomisu7 <dio8ssx@outlook.com>
2024-12-11 06:22:03 +0800

    Modify stage.cpp

--- stage.cpp
+++ stage.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Stop argue whether once room green.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
