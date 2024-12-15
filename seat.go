9cea8bb02374f6250681443b3a1833bb02474e2c
xiaomisu7 <dio8ssx@outlook.com>
2024-12-15 19:14:58 +0800

    Modify seat.go

--- seat.go
+++ seat.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Trouble development catch social small.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
