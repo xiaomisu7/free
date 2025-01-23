01f72ec28655cbedb2bfed0082dc92e13ee6135f
xiaomisu7 <dio8ssx@outlook.com>
2025-01-23 13:18:54 +0800

    Modify week.cpp

--- week.cpp
+++ week.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Market economy play occur few million.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
