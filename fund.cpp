2dc1f7abb1485e9424812fd7c58f3b000f7def43
xiaomisu7 <dio8ssx@outlook.com>
2025-02-19 20:59:33 +0800

    Modify fund.cpp

--- fund.cpp
+++ fund.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# From southern both close trial three conference.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
